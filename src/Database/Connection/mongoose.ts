6509d8fabb0827bf00cb0a760704dafd98f158d2
Ambuj Singh <ambujsingh@Ambujs-MacBook-Air.local>
2025-02-22 13:04:04 +0530

    ad

--- src/Database/Connection/mongoose.ts
+++ src/Database/Connection/mongoose.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
+    console.log('Connecting to MongoDB');
     await mongoose.connect(`${process.env.DATABASE_URI}`);
     console.log('Connected to MongoDB');
   } catch (error) {
